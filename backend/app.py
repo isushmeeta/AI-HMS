ea7fcf51bd91cbcc059ae44782d355a24dd1ac40
isushmeeta <sushmitah0199@gmail.com>
2026-01-17 14:16:23 +0600

    Update Doctor Route

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,8 @@ from flask_cors import CORS
 from config import Config
 from models import db
 from routes.patient_routes import patient_bp
+from routes.doctor_routes import doctor_bp
+from routes.appointment_routes import appointment_bp
 
 def create_app():
     app = Flask(__name__)
@@ -12,6 +14,8 @@ def create_app():
     db.init_app(app)
 
     app.register_blueprint(patient_bp, url_prefix='/api')
+    app.register_blueprint(doctor_bp, url_prefix='/api')
+    app.register_blueprint(appointment_bp, url_prefix='/api')
 
     with app.app_context():
         db.create_all()
